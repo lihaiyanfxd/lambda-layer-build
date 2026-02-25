65495fcd6950b8c8af37fdc91ded46e79d44abc2
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-02-25 16:58:09 +0900

    Update models.py

--- 极简版/app/models.py
+++ 极简版/app/models.py
@@ -2,7 +2,8 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
-class User(SQLModel, table=True):
+class MyTable(SQLModel, table=True):
+    __tablename__ = "my_table"   # 指定真实表名
+
     id: Optional[int] = Field(default=None, primary_key=True)
-    email: str
-    is_active: bool = True
+    name: str
