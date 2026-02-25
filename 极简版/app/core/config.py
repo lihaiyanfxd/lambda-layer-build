fe906abf11e3c6b4c09e2baed494c188b7f62aa6
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-02-25 16:42:54 +0900

    Update config.py

--- 极简版/app/core/config.py
+++ 极简版/app/core/config.py
@@ -2,19 +2,14 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    ENVIRONMENT: str = "production"
-
     POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
 
-    SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-
     @property
-    def SQLALCHEMY_DATABASE_URI(self) -> str:
+    def DATABASE_URI(self) -> str:
         return (
             f"postgresql+psycopg://"
             f"{self.POSTGRES_USER}:"
