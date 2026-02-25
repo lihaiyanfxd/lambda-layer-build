8677168755cbf0f387f60be27ad222a75583f462
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-02-25 16:58:54 +0900

    Update crud.py

--- 极简版/app/crud.py
+++ 极简版/app/crud.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, select
-from app.models import User
+from app.models import MyTable
 
 
 def get_user_by_id(session: Session, user_id: int):
-    statement = select(User).where(User.id == user_id)
+    statement = select(MyTable).where(MyTable.id == user_id)
     return session.exec(statement).first()
