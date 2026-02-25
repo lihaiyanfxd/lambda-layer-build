e3987e7e48eacb298f7b42c9c546062885af2c85
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-02-25 16:43:22 +0900

    Update db.py

--- 极简版/app/core/db.py
+++ 极简版/app/core/db.py
@@ -1,8 +1,8 @@
-from sqlmodel import Session, create_engine
+from sqlmodel import create_engine, Session
 from app.core.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URI,
+    settings.DATABASE_URI,
     pool_pre_ping=True,
     pool_recycle=300,
 )
