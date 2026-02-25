f35f1ac7a5b4925ddb4822eb14c8fa44ea65d986
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-02-25 17:25:28 +0900

    Create config.py

---
+++ psycopg3版本/app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql://"
+            f"{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@"
+            f"{self.POSTGRES_SERVER}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
+
+
+settings = Settings()
