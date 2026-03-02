4b57c6c1c4f1a468aad2bd9c7740839b9f78cf67
lihaiyanfxd <47878289+lihaiyanfxd@users.noreply.github.com>
2026-03-02 20:44:38 +0900

    Initialize logging and add middleware and handlers
    Added logging setup, middleware, and exception handlers.

--- psycopg3版本/lambda_function.py
+++ psycopg3版本/lambda_function.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 from app.api.users import router as users_router
+from app.core.logger import setup_logging
+from app.core.logger import request_logging_middleware
+from app.core.exceptions import register_exception_handlers
+
+
+# 初始化 logging
+setup_logging()
 
 app = FastAPI()
 
+# request logging middleware
+app.middleware("http")(request_logging_middleware)
+
+# router
 app.include_router(users_router)
+
+# exception handler
+register_exception_handlers(app)
 
 handler = Mangum(app)
